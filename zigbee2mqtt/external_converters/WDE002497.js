c6e85cc28610bd078e8c7d091a4175bd31000dd5
Sami Viitanen <sami.viitanen@mavericks.fi>
2025-11-26 10:47:11 +0200

    Add missing energy measurement logic
    - This adds the missing measurement logic to WDE002497

--- zigbee2mqtt/external_converters/WDE002497.js
+++ zigbee2mqtt/external_converters/WDE002497.js
@@ -25,6 +25,8 @@ export default {
             tz.thermostat_temperature_display_mode,
         ],
     exposes: [
+        e.power(),
+        e.energy(),
         e.binary('keypad_lockout', ea.STATE_SET, 'lock1', 'unlock').withDescription('Enables/disables physical input on the device'),
         e.enum('schneider_pilot_mode', ea.ALL, ['contactor', 'pilot']).withDescription('Controls piloting mode'),
         e.enum('temperature_display_mode', ea.ALL, ['celsius', 'fahrenheit']).withDescription('The temperature format displayed on the thermostat screen'),
@@ -40,14 +42,20 @@ export default {
     configure: async (device, coordinatorEndpoint) => {
         const endpoint1 = device.getEndpoint(1);
         const endpoint2 = device.getEndpoint(2);
+        const endpoint5 = device.getEndpoint(5);
         await reporting.bind(endpoint1, coordinatorEndpoint, ['hvacThermostat']);
         await reporting.thermostatPIHeatingDemand(endpoint1);
         await reporting.thermostatOccupiedHeatingSetpoint(endpoint1);
         await reporting.bind(endpoint2, coordinatorEndpoint, ['msTemperatureMeasurement']);
         await reporting.temperature(endpoint2);
         await endpoint1.read('hvacUserInterfaceCfg', ['keypadLockout', 'tempDisplayMode']);
+        await reporting.bind(endpoint5, coordinatorEndpoint, ['seMetering']);
+        await reporting.instantaneousDemand(endpoint5, {min: 0, max: 60, change: 1});
+        await reporting.readMeteringMultiplierDivisor(endpoint5);
+        await reporting.currentSummDelivered(endpoint5, {min: 0, max: 60, change: 1});
     },
     extend: [
+        m.deviceEndpoints({endpoints: {l1: 10, l2: 11}}),
         m.numeric({
             name: 'display_brightness_active',
             cluster: 'hvacUserInterfaceCfg',
@@ -58,6 +66,7 @@ export default {
             valueMin: 0,
             valueMax: 100,
             valueStep: 1,
+            access: "ALL",
             zigbeeCommandOptions: {manufacturerCode: Zcl.ManufacturerCode.SCHNEIDER_ELECTRIC},
         }),
         m.numeric({
@@ -70,6 +79,7 @@ export default {
             valueMin: 0,
             valueMax: 100,
             valueStep: 1,
+            access: "ALL",
             zigbeeCommandOptions: {manufacturerCode: Zcl.ManufacturerCode.SCHNEIDER_ELECTRIC},
         }),
         m.numeric({
@@ -82,6 +92,7 @@ export default {
             valueMin: 5,
             valueMax: 600,
             valueStep: 5,
+            access: "ALL",
             zigbeeCommandOptions: {manufacturerCode: Zcl.ManufacturerCode.SCHNEIDER_ELECTRIC},
         }),
         m.enumLookup({
@@ -91,6 +102,20 @@ export default {
             attribute: {ID: 0xE213, type: Zcl.DataType.UINT8},
             description: 'Control type',
             entityCategory: 'config',
+            access: "ALL",
+            zigbeeCommandOptions: {manufacturerCode: Zcl.ManufacturerCode.SCHNEIDER_ELECTRIC},
+        }),
+        m.numeric({
+            name: 'fixed_load_demand',
+            cluster: 'seMetering',
+            attribute: {ID: 0x4510, type: Zcl.DataType.UINT24},
+            description: 'Fixed load demand',
+            entityCategory: 'config',
+            unit: 'W',
+            valueMin: 0,
+            valueMax: 10000,
+            valueStep: 1,
+            access: "ALL",
             zigbeeCommandOptions: {manufacturerCode: Zcl.ManufacturerCode.SCHNEIDER_ELECTRIC},
         })
     ]
